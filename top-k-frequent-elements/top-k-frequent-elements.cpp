389b75137c8a3df82916932c0ce647c6d3d94266
Andrew Varela <61606497+varelandrew@users.noreply.github.com>
2021-10-12 16:41:34 -0700

    Update top-k-frequent-elements.cpp

--- top-k-frequent-elements/top-k-frequent-elements.cpp
+++ top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -3,42 +3,20 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
          unordered_map<int,int> map;
-        for(int num : nums){
-            map[num]++;
-        }
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++)
+            map[nums[i]]++;
         
-        vector<int> res;
-        // pair<first, second>: first is frequency,  second is number
-        priority_queue<pair<int,int>> pq; 
-        for(auto it = map.begin(); it != map.end(); it++){
-            pq.push(make_pair(it->second, it->first));
-            if(pq.size() > (int)map.size() - k){
-                res.push_back(pq.top().second);
-                pq.pop();
+        priority_queue<pair<int,int>> que; 
+        for(auto it = map.begin(); it != map.end(); it++)
+        {
+            que.push(make_pair(it->second, it->first));
+            if(que.size() > (int)map.size() - k)
+            {
+                ans.push_back(que.top().second);
+                que.pop();
             }
         }
-        return res;
+        return ans;
     }
 };
-
-/*class Solution {
-public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> map;
-        for(int num : nums){
-            map[num]++;
-        }
-        
-        vector<int> res;
-        // pair<first, second>: first is frequency,  second is number
-        priority_queue<pair<int,int>> pq; 
-        for(auto it = map.begin(); it != map.end(); it++){
-            pq.push(make_pair(it->second, it->first));
-            if(pq.size() > (int)map.size() - k){
-                res.push_back(pq.top().second);
-                pq.pop();
-            }
-        }
-        return res;
-    }
-};*/
